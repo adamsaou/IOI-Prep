cf5e94be035ed441229331821006c4ab94336659
adamsaou <saoudani.adam@gmail.com>
2026-05-28 23:53:32 +0100

    Spent day doing SAT prep so not a lot of progress sadly

--- C++_Practise/JuniorSheet/Games.cpp
+++ C++_Practise/JuniorSheet/Games.cpp
@@ -7,10 +7,24 @@ int main(){
     ios::sync_with_stdio(false);cin.tie(NULL);
 
     int n; cin>>n;
+    vector<int> h(n);
+    vector<int> a(n);
+    int ans = 0;
 
-    while(n--){
-        
+
+    for (int i = 0; i < n; i++){
+        cin>>h[i];
+        cin>>a[i];
     }
     
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; i++){
+            if (a[i] == h[j]){
+                ans++;
+            }
+        }
+    }
+
+    cout<<ans;
     return 0;
 }
