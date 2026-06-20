7b437899c1a9426898fa25ecf371afcd8441732a
adamsaou <saoudani.adam@gmail.com>
2026-06-20 15:14:36 +0100

    I will lookk at solution bruh

--- C++_Practise/CSES/PalindromeReorder.cpp
+++ C++_Practise/CSES/PalindromeReorder.cpp
@@ -6,15 +6,27 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL);
-
     string s; cin>>s;
     map <char, int> freq;
 
+    
+    //counts frequency of each letter
     for(char c: s){
         freq[c]++;
+    // }
+    // for(auto p : freq){
+    //     cout << p.first << " : " << p.second << "\n";
     }
-    for(auto p : freq){
-        cout << p.first << " : " << p.second << "\n";
+
+    int min_freq = INT_MAX;
+    int min_freq_element = -1;
+
+    for (const auto& pair: freq){
+        if(pair.second < min_freq){
+            min_freq = pair.second;
+            min_freq_element = pair.first;
+        }
+
     }
 
     //bool exists = true;
@@ -22,12 +34,39 @@ int main(){
     for(auto p : freq){
         if(p.second % 2 != 0){
             uneven++;
-        } else if(uneven > 1){
+        }
+        if(uneven > 1){
             cout << "NO SOLUTION" << "\n";
             return 0;
         }
-        //cout << p.first << " : " << p.second << "\n";
+        cout << p.first << " : " << p.second << "\n";
     }
+    //check if it's possible to make a palindroe out of that string
+
+
+    sort(s.begin(),s.end());
+    cout << s<< "\n";
+    string first;
+    string m;
+    //making a const alg to make the palindrome    
+    //AAAACACBA
+    
+    for(int i = 0;i<s.length();i++){
+        first.push_back(freq.first);
+    }
+
+    //idea: sort from A-Z
+    //AAAAAABCC
+    //construct 2 substrings:
+
+
+    //AACABACAA
+
+    for(int i = 0;i<(s.length() / 2);i++){
+
+    }
+
+    string last;
 
     
     return 0;
